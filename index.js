952c194a7d6308644bb29ac48c8e9618af88c3d0
HarshitaVastrad <51442066+HarshitaVastrad@users.noreply.github.com>
2019-06-17 12:35:55 +0530

    message

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 var express=require('express');
 var app=express();
 const port=3000;
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+
+  var dbo = db.db("mydb");
+  
+        app.post('/go',()=>{
+
+            var myobj = { name: "Company Inc", address: "Highway 37" };
+            
+            {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+            }
+        })
+});
+
+
+
+
 app.get('/',(req,res) =>{
     res.send('hey This is Harshita');
 });
-app.listen(port)
+app.listen(port,()=>{
+    console.log('Server is running on port ',port);
+})
